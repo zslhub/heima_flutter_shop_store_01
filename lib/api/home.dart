67cd90fd083fe9b6ceddb6e6e4de89c20f17ecfc
zsl <2191055003@qq.com>
2025-12-02 16:38:55 +0800

    无限滚动列表的上拉加载

--- lib/api/home.dart
+++ lib/api/home.dart
@@ -44,3 +44,16 @@ Future<SpecialRecommend> getOneStopListAPI() async {
     await dioRequest.get(HttpConstants.ONE_STOP_LIST),
   );
 }
+
+// 推荐列表
+Future<List<GoodDetailItem>> getRecommendListAPI(
+  Map<String, dynamic> params,
+) async {
+  // 返回请求
+  return ((await dioRequest.get(HttpConstants.RECOMMEND_LIST, params: params))
+          as List)
+      .map((item) {
+        return GoodDetailItem.formJSON(item as Map<String, dynamic>);
+      })
+      .toList();
+}
--- lib/viewmodels/home.dart
+++ lib/viewmodels/home.dart
@@ -149,3 +149,27 @@ class SpecialRecommend {
   }
 }
 
+class GoodDetailItem extends GoodsItem {
+  int payCount = 0;
+
+  /// 商品详情项
+  GoodDetailItem({
+    required super.id,
+    required super.name,
+    required super.price,
+    required super.picture,
+    required super.orderNum,
+    required this.payCount,
+  }) : super(desc: "");
+  // 转化方法
+  factory GoodDetailItem.formJSON(Map<String, dynamic> json) {
+    return GoodDetailItem(
+      id: json["id"]?.toString() ?? "",
+      name: json["name"]?.toString() ?? "",
+      price: json["price"]?.toString() ?? "",
+      picture: json["picture"]?.toString() ?? "",
+      orderNum: int.tryParse(json["orderNum"]?.toString() ?? "0") ?? 0,
+      payCount: int.tryParse(json["payCount"]?.toString() ?? "0") ?? 0,
+    );
+  }
+}
